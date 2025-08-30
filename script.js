2325ca80a60518f57c4a9b3ee6b1c6363d9cdd2b
Shasank Shrestha <shresthashasank59@gmail.com>
2025-08-30 13:18:45 +0545

    custom cursor

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ themeToggle.addEventListener("click", () => {
     localStorage.setItem("theme", "dark")
   }
 })
+
+
+onst cursor = document.querySelector(".cursor")
+const cursorFollower = document.querySelector(".cursor-follower")
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px"
+  cursor.style.top = e.clientY + "px"
+
+  setTimeout(() => {
+    cursorFollower.style.left = e.clientX + "px"
+    cursorFollower.style.top = e.clientY + "px"
+  }, 100)
+})
